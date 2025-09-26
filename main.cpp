1ad73ce8b66ea5f5cfc5bf0630e640b4520759ff
Vasiliy Samoylik <44618834+SamoylikV@users.noreply.github.com>
2025-09-26 21:23:21 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostring>
+#include <cstdlib>
+#include <string>
+
+int main(){
+  const char* secret = std::getenv("PASSWORD");
+  std::string s(secret);
+  if (s = "qwerty"){
+    std::cout << "Пароль верный!!!" << std::endl;
+  } eles {
+    std::cout << "Пароль неверный!!!" << std::endl;
+  }
+  return 0;
+}
